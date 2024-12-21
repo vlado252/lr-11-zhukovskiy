9b320eec6f9b89d734b33670b25ffe8a297ca932
vlado252 <vladgremlin1337@gmail.com>
2024-12-21 10:32:31 +0300

    first commit

--- transport/client.py
+++ transport/client.py
@@ -4,7 +4,7 @@ class Client():
         if isinstance(cargo_weight, (int, float)) and cargo_weight > 0:
             self.name = name
             self.cargo_weight = cargo_weight
-            self.is_vip = is_vip
+            self.is_vip = is_vip 
         else:
             raise ValueError("Вес груза должно быть положительным числом!")
             
